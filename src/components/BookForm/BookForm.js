9b4de51dd844bd5f37c40e905ff2911159965455
Maciej Tabiś <maciek.tabis@gmail.com>
2023-01-29 22:22:43 +0100

    Use of class components in app

--- src/components/BookForm/BookForm.js
+++ src/components/BookForm/BookForm.js
@@ -1,32 +1,43 @@
-import {useState} from 'react';
-const BookForm = ({addBook}) => {
-	const [title, setTitle] = useState('');
-	const [author, setAuthor] = useState('');
-
-	const handleSubmit = (e) => {
+import React, {useState} from 'react';
+class BookForm extends React.Component {
+	state = {
+		title: '',
+		author: '',
+	};
+	handleSubmit = (e) => {
 		e.preventDefault();
-		addBook({title, author});
-		setTitle('');
-		setAuthor('');
+		this.props.addBook({title: this.state.title, author: this.state.author});
+		this.setState({...this.state, title: '', author: ''});
 	};
 
-	return (
-		<form onSubmit={handleSubmit}>
-			Title:
-			<input
-				type='text'
-				value={title}
-				onChange={(e) => setTitle(e.target.value)}
-			></input>
-			Author:
-			<input
-				type='text'
-				value={author}
-				onChange={(e) => setAuthor(e.target.value)}
-			></input>
-			<button>Add book</button>
-		</form>
-	);
-};
+	setTitle = (value) => {
+		this.setState({...this.state, title: value});
+	};
+	setAuthor = (value) => {
+		this.setState({...this.state, author: value});
+	};
+
+	render() {
+		const {handleSubmit, setTitle, setAuthor} = this;
+		const {title, author} = this.state;
+		return (
+			<form onSubmit={handleSubmit}>
+				Title:
+				<input
+					type='text'
+					value={title}
+					onChange={(e) => setTitle(e.target.value)}
+				></input>
+				Author:
+				<input
+					type='text'
+					value={author}
+					onChange={(e) => setAuthor(e.target.value)}
+				></input>
+				<button>Add book</button>
+			</form>
+		);
+	}
+}
 
 export default BookForm;
--- src/components/BooksList/BooksList.js
+++ src/components/BooksList/BooksList.js
@@ -1,14 +1,18 @@
-const BooksList = ({books, removeBook}) => {
-	return (
-		<ul>
-			{books.map((book) => (
-				<li key={book.id}>
-					{book.title} by {book.author}
-					<button onClick={() => removeBook(book.id)}>Remove</button>
-				</li>
-			))}
-		</ul>
-	);
-};
+import React from 'react';
+
+class BooksList extends React.Component {
+	render() {
+		return (
+			<ul>
+				{this.props.books.map((book) => (
+					<li key={book.id}>
+						{book.title} by {book.author}
+						<button onClick={() => this.props.removeBook(book.id)}>Remove</button>
+					</li>
+				))}
+			</ul>
+		);
+	}
+}
 
 export default BooksList;
